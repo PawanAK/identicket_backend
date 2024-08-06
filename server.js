bb43c4439f9028953f9e5b591dc1de62915a908a
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-08-07 00:49:07 +0530

    coorrss

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 
 // User model
